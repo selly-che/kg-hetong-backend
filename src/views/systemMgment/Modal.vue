e46248f4afdd6e2431c82b32b4c0cefc57558a5c
199380 <19938032985@163.com>
2026-01-09 16:51:58 +0800

    系统管理1.0

---
+++ src/views/systemMgment/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <a-modal v-model:visible="visible" title="编辑用户" @ok="handleOk">
+      <template v-slot:footer>
+        <a-button key="back" @click="handleCancel"> 取消 </a-button>
+        <a-button
+          key="submit"
+          type="primary"
+          :loading="loading"
+          @click="handleOk"
+        >
+          提交
+        </a-button>
+      </template>
+      <slot>暂无内容</slot>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      visible: false,
+      currentRecord: null,
+    };
+  },
+  methods: {
+    showModal(record) {
+      this.currentRecord = record;
+      this.visible = true;
+    },
+    handleOk() {
+      this.loading = true;
+      setTimeout(() => {
+        this.visible = false;
+        this.loading = false;
+        this.currentRecord = null;
+      }, 3000);
+    },
+    handleCancel() {
+      this.visible = false;
+      this.currentRecord = null;
+    },
+  },
+};
+</script>
--- src/views/systemMgment/index.vue
+++ src/views/systemMgment/index.vue
@@ -1,28 +1,64 @@
 <template>
-    <div class="peijianSort">
-       系统管理
+  <div class="container">
+    <div class="title">系统管理</div>
+    <span>用户管理和角色权限配置</span>
+    <a-popover placement="rightTop">
+      <template v-slot:content>
+        <div
+          class="content"
+          style="
+            width: 300px;
+            height: 300px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: flex-start;
+          "
+        >
+          <div>
+            <p>系统管理员:</p>
+            <p>所有权限<br />用户管理<br />系统设置<br />数据备份</p>
+          </div>
+          <div>
+            <p>分管领导：</p>
+            <p>合同审批<br />项目查看<br />统计查询<br />下属管理</p>
+          </div>
+          <div>
+            <p>部门主管：</p>
+            <p>部门管理<br />合同查看<br />项目查看<br />统计查询</p>
+          </div>
+          <div>
+            <p>一般使用人员：</p>
+            <p>合同查看<br />项目查看<br />基础查询</p>
+          </div>
+          <div>
+            <p>合同管理员：</p>
+            <p>合同管理<br />合同录入<br />台账管理<br />统计查询</p>
+          </div>
+        </div>
+      </template>
+      <QuestionCircleTwoTone />
+    </a-popover>
+    <div class="context">
+      <Context />
     </div>
+  </div>
 </template>
-<script lang="ts" setup>
-import { onMounted } from 'vue';
-
-onMounted(() => {
-    console.log('组件挂载完成');
-    adc(1);
-});
-
-const adc = (item :any) => {
-    console.log("item",JSON.parse(JSON.stringify(item)));
-}
+<script setup>
+import Context from "./Context.vue";
+import { QuestionCircleTwoTone } from "@ant-design/icons-vue";
 </script>
 
-<style lang="less" scoped> 
-.peijianSort {
-    padding: 1rem;
-    // border: 1px solid #ebeef5;
-    background-color: white;
-    // margin-top: 1rem;
-    margin: 1rem;
-
-}   
+<style lang="less" scoped>
+.title {
+  font-size: 16px;
+  font-weight: bold;
+  color: #333333;
+}
+.context {
+  width: 100%;
+  height: 600px;
+  background-color: #ffffff;
+}
 </style>
