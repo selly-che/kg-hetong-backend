6adcec12f095ce7624fe51bd1180e49f61b1d522
199380 <19938032985@163.com>
2026-02-26 18:01:00 +0800

    项目概况

--- src/network/apis/project.ts
+++ src/network/apis/project.ts
@@ -1,11 +1,19 @@
 export default {
-  // 获取项目列表信息
+  // // 分页查询项目列表信息
   ///jeecg-boot/projectcontract/tpmProject/list
   GetProjectList(params: any) {
     return {
       method: "GET",
       url: "/projectcontract/tpmProject/list",
-      data: params,
+      params,
+    };
+  },
+  ///jeecg-boot/projectcontract/tpmProject/details
+  GetProjectDetails(params: any) {
+    return {
+      method: "GET",
+      url: "/projectcontract/tpmProject/details",
+      params,
     };
   },
 };
