64920ca450b1fbda8bb419c119f75c4d6beb2b6c
199380 <19938032985@163.com>
2026-03-13 17:17:24 +0800

    收款新增

---
+++ src/network/apis/zhangdan.ts
@@ -0,0 +1,33 @@
+export default {
+  //删除/jeecg-boot/bill/delete
+  Delete(params: string) {
+    return {
+      method: "DELETE",
+      url: "/bill/delete",
+      params,
+    };
+  },
+  //查询单条/jeecg-boot/bill/getById
+  GetById(params: string) {
+    return {
+      method: "GET",
+      url: "/bill/getById",
+      params,
+    };
+  },
+  //新增/jeecg-boot/bill/save
+  Save(params: any) {
+    return {
+      method: "POST",
+      url: "/bill/save",
+      data: params,
+    };
+  },
+  //列表查询/jeecg-boot/bill/queryBillList
+  QueryBillList() {
+    return {
+      method: "GET",
+      url: "/bill/queryBillList",
+    };
+  },
+};
